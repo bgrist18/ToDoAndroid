c68e2e878db7ba5f1c8c550c48bc7a10b92159cd
bgrist18 <bgrist309@gmail.com>
2016-09-28 12:27:15 -0400

    asdf

--- app/src/main/java/com/theironyard/todoandroid/ToDoItem.java
+++ app/src/main/java/com/theironyard/todoandroid/ToDoItem.java
@@ -1,8 +1,24 @@
 package com.theironyard.todoandroid;
 
 /**
- * Created by brettgrist on 9/28/16.
+ * Created by brett on 9/28/16.
  */
 
 public class ToDoItem {
+    String text;
+    boolean isDone;
+
+    public ToDoItem(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+
+    @Override
+    public String toString() {
+        String checkbox = " ";
+        if (isDone) {
+            checkbox = "✓";
+        }
+        return String.format("%s %s", checkbox, text);
+    }
 }
